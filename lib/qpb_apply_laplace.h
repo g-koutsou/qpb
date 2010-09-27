429e06e70d4398bddf543ec50f2132400f0bc880
Giannis Koutsou <koutsou@upquark.(none)>
2010-09-27 15:13:51 +0200

    Deleted some files

--- lib/qpb_apply_laplace.h
+++
@@ -1,5 +0,0 @@
-#ifndef _QPB_APPLY_LAPLACE_H
-#define _QPB_APPLY_LAPLACE_H 1
-#include <qpb_types.h>
-void qpb_apply_laplace(qpb_spinor_field, qpb_spinor_field, qpb_gauge_field);
-#endif /* _QPB_APPLY_LAPLACE_H */
