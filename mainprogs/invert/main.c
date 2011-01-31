8b649ed8f47f62e2dc502f493541ab52cdb271dd
Giannis Koutsou <koutsou@upquark.(none)>
2011-01-31 11:56:04 +0200

    Options to APE smear and shift in invert main program

--- mainprogs/invert/main.c
+++ mainprogs/invert/main.c
@@ -111,6 +111,48 @@ main(int argc, char *argv[])
     case QPB_RAND:
       break;
     }
+
+  qpb_double ape_alpha;
+  if(sscanf(qpb_parse("APE alpha"), "%lf", &ape_alpha)!=1)
+    {
+      error("error parsing for %s\n",
+	    "alpha");
+      exit(QPB_PARSER_ERROR);
+    }
+
+  int ape_niter;
+  if(sscanf(qpb_parse("APE iterations"), "%d", &ape_niter)!=1)
+    {
+      error("error parsing for %s\n",
+	    "Smear iterations");
+      exit(QPB_PARSER_ERROR);
+    }
+
+  int shifts[ND];
+  if(sscanf(qpb_parse("Gauge shifts"), "%d %d %d %d",
+	    shifts, shifts+1, shifts+2, shifts+3)!=ND)
+    {
+      error("error parsing for %s\n", 
+	      "Shifts");
+      exit(QPB_PARSER_ERROR);
+    }
+  if(shifts[0]<0 ||
+     shifts[1]<0 ||
+     shifts[2]<0 ||
+     shifts[3]<0)
+    {
+      error("only provide positive shifts, quiting\n");
+      exit(QPB_PARAMETERS_ERROR);
+    }
+
+  if(shifts[0] > g_dim[0]-1 ||
+     shifts[1] > g_dim[1]-1 ||
+     shifts[2] > g_dim[2]-1 ||
+     shifts[3] > g_dim[3]-1)
+    {
+      error("shift(s) go beyond lattice length(s), quiting\n");
+      exit(QPB_PARAMETERS_ERROR);
+    }
   
   unsigned int seed;
   if(sscanf(qpb_parse("Random seed"), "%u", &seed)!=1)
@@ -260,6 +302,24 @@ main(int argc, char *argv[])
 	problem_params.g_dim[2], 
 	problem_params.g_dim[3]);
   print(" Processes = (1,%2d,%2d,%2d)\n", procs[0], procs[1], procs[2]);
+  switch(conf_opt)
+    {
+    case QPB_ZERO:
+      print(" Gauge field = Zeros\n");
+      break;
+    case QPB_UNIT:
+      print(" Gauge field = Unit\n");
+      break;
+    case QPB_FILE:
+      print(" Gauge field = %s\n", conf_file);
+      break;
+    case QPB_RAND:
+      print(" Gauge field = Random\n");
+      break;
+    }
+  print(" APE alpha = %g\n", ape_alpha);
+  print(" APE iterations = %d\n", ape_niter);
+  print(" Conf shifts = %d %d %d %d\n", shifts[0], shifts[1], shifts[2], shifts[3]);
   print(" kappa = %g\n", kappa);
   print(" Clover param = %g\n", c_sw);
   print(" Solver epsilon = %e\n", epsilon);
@@ -295,6 +355,22 @@ main(int argc, char *argv[])
   /* Calculate plaquette */
   qpb_double plaquette = qpb_plaquette(gauge);
   print(" Plaquette = %12.8f\n", plaquette);
+
+  /* APE smear the gauge field */
+  if(ape_niter != 0)
+    {
+      qpb_gauge_field apegauge = qpb_gauge_field_init();
+      print(" APE smear gauge field...\n");
+      qpb_apesmear_niter(apegauge, gauge, ape_alpha, ape_niter);
+      qpb_gauge_field_copy(gauge, apegauge);
+      qpb_gauge_field_finalize(apegauge);
+
+      plaquette = qpb_plaquette(gauge);
+      print(" Plaquette = %12.8f\n", plaquette);
+    }
+
+  /* Shift it */
+  qpb_gauge_field_shift(gauge, shifts);
 
   /* Clover term */
   qpb_clover_term clover_term = qpb_clover_term_init();
