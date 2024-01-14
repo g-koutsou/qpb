652021bdf3813415151607b8179cc620de45141b
Stylianos Gregoriou <s.gregoriou@cyi.ac.cy>
2024-01-14 21:36:13 +0200

    updated function names

--- lib/qpb_overlap_Chebyshev.h
+++ lib/qpb_overlap_Chebyshev.h
@@ -1,3 +1,6 @@
+/* NOTE: The definitions of all functions are included allowing this way flexibility
+     in the order of functions within the script. */
+
 #ifndef _QPB_OVERLAP_CHEBYSHEV_H
 #define _QPB_OVERLAP_CHEBYSHEV_H 1
 
@@ -9,15 +12,13 @@ void qpb_overlap_Chebyshev_finalize();
 
 void qpb_massive_overlap_Chebyshev(qpb_spinor_field, qpb_spinor_field);
 void qpb_massless_overlap_Chebyshev(qpb_spinor_field, qpb_spinor_field);
-void qpb_Chebyshev_sign_function_of_H(qpb_spinor_field, qpb_spinor_field);
+void qpb_gamma5_sign_function_of_H(qpb_spinor_field, qpb_spinor_field);
 void qpb_Chebyshev_polynomial_term_without_caching(qpb_spinor_field, qpb_spinor_field, int);
 void qpb_Chebyshev_polynomial_term(qpb_spinor_field, qpb_spinor_field, int);
 void qpb_overlap_X(qpb_spinor_field, qpb_spinor_field);
-void qpb_overlap_H(qpb_spinor_field, qpb_spinor_field);
-void qpb_overlap_kernel(qpb_spinor_field, qpb_spinor_field);
 
 qpb_complex expansion_Chebyshev_coeff_n(int);
-qpb_double Chebyshev_r(qpb_double);
+qpb_double scalar_r_func(qpb_double);
 qpb_double scalar_Chebyshev_polynomial_term(qpb_double, int);
 qpb_double scalar_Chebyshev_polynomial_term_with_caching(int, int);
 
