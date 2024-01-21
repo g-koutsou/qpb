7f1d6174349d359050d89bacd75b08f4d5f5addb
Stylianos Gregoriou <s.gregoriou@cyi.ac.cy>
2024-01-21 15:11:03 +0200

    reduced the number of function prototypes to the bare minimum

--- lib/qpb_overlap_Chebyshev.h
+++ lib/qpb_overlap_Chebyshev.h
@@ -1,28 +1,12 @@
-/* NOTE: The definitions of all functions are included allowing this way flexibility
-     in the order of functions within the script. */
-
 #ifndef _QPB_OVERLAP_CHEBYSHEV_H
 #define _QPB_OVERLAP_CHEBYSHEV_H 1
 
 #include <qpb_types.h>
 #include <qpb_overlap_Chebyshev_defs.h>
 
+void qpb_overlap_Chebyshev(qpb_spinor_field, qpb_spinor_field);
+
 void qpb_overlap_Chebyshev_init(void *, qpb_clover_term, qpb_double, qpb_double, qpb_double, qpb_double, int, int);
 void qpb_overlap_Chebyshev_finalize();
-
-void qpb_massive_overlap_Chebyshev(qpb_spinor_field, qpb_spinor_field);
-void qpb_massless_overlap_Chebyshev(qpb_spinor_field, qpb_spinor_field);
-void qpb_gamma5_sign_function_of_H(qpb_spinor_field, qpb_spinor_field);
-void qpb_Chebyshev_polynomial_term_without_caching(qpb_spinor_field, qpb_spinor_field, int);
-void qpb_Chebyshev_polynomial_term(qpb_spinor_field, qpb_spinor_field, int);
-void qpb_overlap_X(qpb_spinor_field, qpb_spinor_field);
-
-qpb_complex expansion_Chebyshev_coeff_n(int);
-qpb_double scalar_r_func(qpb_double);
-qpb_double scalar_Chebyshev_polynomial_term(qpb_double, int);
-qpb_double scalar_Chebyshev_polynomial_term_with_caching(int, int);
-
-void tridiag_eigenv(double *, double *, double *, int);
-void qpb_extreme_eigenvalues_of_H_squared(qpb_double *, qpb_double *, qpb_double, int);
 
 #endif /* _QPB_OVERLAP_CHEBYSHEV_H */
